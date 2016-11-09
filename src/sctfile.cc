bd757f0b9b64c14f26b91b6749338dac0c65b9c6
Wei Zhu <wxz138@case.edu>
2016-11-08 19:55:05 -0500

    minor

--- src/sctfile.cc
+++ src/sctfile.cc
@@ -2,3 +2,21 @@
 #include <string>
 #include <sstream>
 #include <vector>
+
+
+// void Menu::readFile() {
+//     string line;
+//     vector <string> lines;
+//     ifstream myfile ("file.txt");
+
+//     if (myfile.is_open()) {
+//         while (getline (myfile, line)) {
+//           cout << line << "\n";
+//           lines.push_back(line);
+//         }
+
+//         myfile.close();
+//     } else { 
+//        cout << "Unable to open file"; 
+//     }
+// }
